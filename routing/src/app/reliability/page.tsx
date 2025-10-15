dd8c1ab439e02d3bd9ff24153b5b4ba28eca185f
Abhay-0103 <ab0321054@gmail.com>
2025-10-15 11:46:06 +0530

    Created Routing

--- routing/src/app/reliability/page.tsx
+++ routing/src/app/reliability/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const ReliabilityPage = () => {
   return (
-    <div>ReliabilityPage</div>
-  )
-}
+    <div>
+      ReliabilityPage
+    </div>
+  );
+};
 
-export default ReliabilityPage
+export default ReliabilityPage;
