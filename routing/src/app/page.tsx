cfddd9f65d8d00368c87bdef316cd5b5c15d0e52
Abhay-0103 <ab0321054@gmail.com>
2025-10-15 12:18:24 +0530

    Added Hero Components

--- routing/src/app/page.tsx
+++ routing/src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroPage from "@/components/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello Bhai</h1>
+      <HeroPage />
     </div>
   );
 }
