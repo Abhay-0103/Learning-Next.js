2b952d84da277d2eb80b62d823ba35c316d4b86b
Abhay-0103 <ab0321054@gmail.com>
2025-10-15 12:18:52 +0530

    Added Link Sections to Navigate

--- routing/src/components/header.tsx
+++ routing/src/components/header.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="absolute z-10 w-full">
       <nav className="container flex items-center justify-between mx-auto h-14">
-        <h1 className="font-bold text-2xl">Home</h1>
+        <Link href="/">
+          <h1 className="font-bold text-2xl">Home</h1>
+        </Link>
         <div className="flex gap-2">
-          <h1>Performance</h1>
-          <h1>Reliability</h1>
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
         </div>
       </nav>
     </div>
